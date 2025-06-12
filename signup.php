7adc7a9fff50c6c1c067ad8f9e3cdb1cd5454406
Pilla <d33gen@outlook.com>
2025-06-12 10:33:09 -0400

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Signup</h2>
+    <form method="post">
+        <input type="email" name="email" required placeholder="Email">
+        <input type="password" name="password" required placeholder="Password">
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
